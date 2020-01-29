5a9b91961085663e5cba1c9fa71db3a90335e74b
samuelroland <samuel.roland@cpnv.ch>
2020-01-29 22:35:05 +0100

    index added action and start session and GET and POST value

--- app/index.php
+++ app/index.php
@@ -5,7 +5,7 @@
  *  Author: Samuel Roland
  *  Creation date: 20.12.2019
  */
-
+session_start();
 require_once "controler/controler.php";
 $title = "Accueil";
 $content = "Contenu de la page $title";
@@ -13,18 +13,37 @@ $action = "";
 if (isset($_GET['action'])) {
     $action = $_GET['action'];
 }
+//id of the chat to display if action is viewchat
+if (isset($_GET['chatid'])) {
+    $chatid = $_GET['chatid'];
+}
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];  //by default the value of $username is the username in the session
+} else {
+    extract($_POST);    //extract every vars in $_POST: $username, $password
+}
+
 
 switch ($action) {
     case "login":
         login($username, $password);
         break;
-        $title = "view chat";
-        viewchat();
+    case "viewchat":
+        $title = "view chat " . $chatid;
+        viewchat($chatid);
+        break;
+    case "convert":
+        //convert();
+        break;
+    case "sendchat":
+        sendchat($username);
+        break;
     case "disconnect":
         disconnect();
         break;
-    case "home":
+    default:
         $title = "Home";
+        home();
         break;
 }
 ?>
